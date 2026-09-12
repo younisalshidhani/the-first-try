e3fedb52f314f863cfe8eb8a52c6cacaa0a23e55
younisalshidhani <younisalshidhani@gmail.com>
2026-09-12 22:02:00 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-// Service Worker لتطبيق تقرير الزيارات الشهرية - v4.3.0
+// Service Worker لتطبيق تقرير الزيارات الشهرية - v5.0.0
 // يُتيح العمل دون إنترنت ويعالج الإشعارات المجدولة
 
-const CACHE_NAME = 'visit-report-v4.9.0';
+const CACHE_NAME = 'visit-report-v5.0.0';
 const CACHE_FILES = [
   './',
   './index.html',
